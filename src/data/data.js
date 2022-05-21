457ef19f0537505c31f3c4b3e4436543bed0c4eb
chriskorsak <chris@chriskorsak.com>
2022-05-21 00:07:51 -0700

    add additional products to data

--- src/data/data.js
+++ src/data/data.js
@@ -1,10 +1,12 @@
 const products = [
   { category: 'Fruits', price: '$1', stocked: true, name: 'Apple' },
-  { category: 'Fruits', price: '$1', stocked: true, name: 'Dragonfruit' },
+  { category: 'Dairy', price: '$4', stocked: false, name: 'Milk' },
   { category: 'Fruits', price: '$2', stocked: false, name: 'Passionfruit' },
   { category: 'Vegetables', price: '$2', stocked: true, name: 'Spinach' },
   { category: 'Vegetables', price: '$4', stocked: false, name: 'Pumpkin' },
+  { category: 'Fruits', price: '$1', stocked: true, name: 'Dragonfruit' },
   { category: 'Vegetables', price: '$1', stocked: true, name: 'Peas' },
+  { category: 'Meat', price: '$8', stocked: true, name: 'Chicken' },
 ];
 
 export default products;
